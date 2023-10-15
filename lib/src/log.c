78fc22e88367a709d3ab082eca500bbf252e9acf
Luca Gasperini <dev@xsoftware.it>
2023-10-15 15:05:38 +0200

    Add datetime on logs

--- lib/src/log.c
+++ lib/src/log.c
@@ -6,10 +6,17 @@
 #include <stdarg.h> // va_list, va_start, va_end
 #include <unistd.h> // fsync
 #include <pthread.h> // pthread_mutex_lock, pthread_mutex_unlock
+#include <time.h>  // tm, time, localtime, time_t
 
 int matrix_log_fd = -1;
 static pthread_mutex_t s_matrix_log_mutex;
 
+static inline void s_matrix_print_datetime(int fd)
+{
+        time_t t = time(NULL);
+        struct tm* tm = localtime(&t);
+        dprintf(fd, "[%d-%02d-%02d %02d:%02d:%02d] ", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
+}
 
 void matrix_log_open(const char* file)
 {
@@ -55,11 +62,15 @@ void matrix_log_debug(const char* msg, ...)
 
         // lock the log mutex
         pthread_mutex_lock(&s_matrix_log_mutex);
+        // print datetime to log
+        s_matrix_print_datetime(matrix_log_fd);
         // set args for matrix log
         va_start(args, msg);
         vdprintf(matrix_log_fd, msg, args);
         va_end(args);
 
+        // print datetime to stdout
+        s_matrix_print_datetime(STDOUT_FILENO);
         // set args for stdout
         va_start(args, msg);
         vdprintf(STDOUT_FILENO, msg, args);
@@ -79,6 +90,8 @@ void matrix_log_info(const char* msg, ...)
         // lock the log mutex
         pthread_mutex_lock(&s_matrix_log_mutex);
 
+        // print datetime to log
+        s_matrix_print_datetime(matrix_log_fd);
         va_start(args, msg);
         vdprintf(matrix_log_fd, msg, args);
         va_end(args);
@@ -94,11 +107,15 @@ void matrix_log_error(const char* msg, ...)
         // lock the log mutex
         pthread_mutex_lock(&s_matrix_log_mutex);
 
+        // print datetime to log
+        s_matrix_print_datetime(matrix_log_fd);
         // set args for matrix log
         va_start(args, msg);
         vdprintf(matrix_log_fd, msg, args);
         va_end(args);
 
+        // print datetime to stderr
+        s_matrix_print_datetime(STDERR_FILENO);
         // set args for stderr
         va_start(args, msg);
         vdprintf(STDERR_FILENO, msg, args);
